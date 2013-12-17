f5e1305c20b1344bcdd82afcede2e7021681c653
Nick Jones <nick@nojones.net>
2013-12-17 02:48:37 +0000

    JSLint passes

--- app.js
+++ app.js
@@ -18,28 +18,26 @@ app.use(orm.express("sqlite://test.db",
                 next();
             });
 
-
-
             models.user = db.define("user",
                 {
                     firstname : String,
                     lastname  : String,
                     email     : String,
-                    password  : String, //bcrypt hashes, need to write a function in here somewhere to manage that, nick to sort
+                    password  : String //bcrypt hashes, need to write a function in here somewhere to manage that, nick to sort
 
                 }, {
                     methods: {
                         fullName: function () {
                             return this.firstname + ' ' + this.lastname;
                         }
-                    },
+                    }
                 });
 
             models.question = db.define("question",
                 {
                     date_posted : Date,
                     content     : String,
-                    title       : String,
+                    title       : String
                 }, {
                 });
 
@@ -48,47 +46,55 @@ app.use(orm.express("sqlite://test.db",
             models.answer = db.define("answer",
                 {
                     content : String,
-                    date_posted : Date,
+                    date_posted : Date
                 }, {
                 });
 
-			models.comments_question = db.define("comments_question",
-				{
-					content : String,
-					date_posted : Date
-				},{	
-				});
+            models.comments_question = db.define("comments_question",
+                {
+                    content : String,
+                    date_posted : Date
+                }, {
+                });
 
-			db.models.comments_question.hasOne("owner", db.models.user, { reverse: 'comments_question' });
-			db.models.comments_question.hasOne("question", db.models.question, { reverse: 'comments' });
+            db.models.comments_question.hasOne("owner", db.models.user, { reverse: 'comments_question' });
+            db.models.comments_question.hasOne("question", db.models.question, { reverse: 'comments' });
 
-			models.comments_answer = db.define("comments_answer",
-				{
-					content : String,
-					date_posted : Date 
-				}, {	
-				});
-				
-			db.models.comments_answer.hasOne("owner", db.models.user, { reverse: 'comments_answer' });
-			db.models.comments_answer.hasOne("answer", db.models.answer, { reverse: 'comments' });
+            models.comments_answer = db.define("comments_answer",
+                {
+                    content : String,
+                    date_posted : Date
+                }, {
+                });
+            db.models.comments_answer.hasOne("owner", db.models.user, { reverse: 'comments_answer' });
+            db.models.comments_answer.hasOne("answer", db.models.answer, { reverse: 'comments' });
+            models.question_vote = db.define("question_vote",
+                {
+                    vote: Boolean
+                }, {
+                });
+            db.models.question_vote.hasOne("owner", db.models.user, { reverse: 'votes' });
+            db.models.question_vote.hasOne("question", db.models.question, { reverse: 'votes' });
+            models.answer_vote = db.define("answer_vote",
+                {
+                    vote: Boolean
+                }, {
+                });
+            db.models.answer_vote.hasOne("owner", db.models.user, { reverse: 'votes' });
+            db.models.answer_vote.hasOne("question", db.models.question, { reverse: 'votes' });
         }
     }));
-	
 /*
 
 AUTHENTICATION CODE
 
 */
 
-function verifyUser (user, password) {
-	if (password == user.password)
-	{
-		var result = true;
-	}
-	else
-	{
-		var result = false;
-	}
+function verifyUser(user, password) {
+    var result = false;
+    if (password === user.password) {
+        result = true;
+    }
 }
 
 app.listen(8888);
@@ -211,7 +217,7 @@ app.post("/users", function (req, res) {
                 lastname: req.body.lastname,
                 email: req.body.email,
                 password: req.body.password
-            }, ],
+            }],
 
             function (err, user_created) {
                 if (!err) {
@@ -343,7 +349,7 @@ app.post("/questions", function (req, res) {
                 title: req.body.title,
                 content: req.body.content,
                 owner_id: 2
-            }, ],
+            } ],
             function (err, question_created) {
                 if (!err) {
                     var question = question_created[0];
@@ -400,19 +406,18 @@ app.get('/questions/comments', function (req, res) {
 });
 
 app.get('/questions/comments/:question_id', function (req, res) {
-	// this gets the question, and then we need to get the comments from that.
+    // this gets the question, and then we need to get the comments from that.
     req.models.question.get(req.params.question_id, function (err, question) {
         if (!err) {
             res.setHeader('Content-Type', 'application/json');
-			
-			// we have the question.
-			question.getComments(function(err, comments) {
-				if(!err) {
-					res.end(JSON.stringify(comments))
-				} else {
-					res.status(404).send('Comment not found');
-				}
-			});
+            // we have the question.
+            question.getComments(function (err, comments) {
+                if (!err) {
+                    res.end(JSON.stringify(comments));
+                } else {
+                    res.status(404).send('Comment not found');
+                }
+            });
         } else {
             res.status(404).send('Comment not found');
         }
@@ -420,21 +425,18 @@ app.get('/questions/comments/:question_id', function (req, res) {
 });
 
 app.get('/questions/comments/:question_id/:comment_id', function (req, res) {
-	// this gets the question, and then we need to get the comments from that.
+    // this gets the question, and then we need to get the comments from that.
     req.models.comments_question.get(req.params.comment_id, function (err, comment) {
         if (!err) {
             res.setHeader('Content-Type', 'application/json');
-			
-			// we have the answer, verify this is from the right question
-			comment.getOwner(function(err, owner) {
-				if(!err) {
-		            res.end(JSON.stringify(comment));
-				}
-				else {
-					res.status(404).send('Comment not found');
-				}
-			});
-			
+            // we have the answer, verify this is from the right question
+            comment.getOwner(function (err, owner) {
+                if (!err) {
+                    res.end(JSON.stringify(comment));
+                } else {
+                    res.status(404).send('Comment not found');
+                }
+            });
         } else {
             res.status(404).send('Comment not found');
         }
@@ -452,25 +454,23 @@ app.put('/questions/comments/:question_id/:comment_id', function (req, res) {
     if (!errors) {
         req.models.comments_question.get(req.params.comment_id, function (err, comment) {
             if (!err) {
-				
-				// verify this comment belongs to the question
-				if(comment.id == req.params.question_id) {
+                // verify this comment belongs to the question
+                if (comment.id === req.params.question_id) {
 
-					// update the content
-	                comment.content = req.body.content;
-					
-	                comment.save(function (err) {
-	                    if (!err) {
-	                        res.setHeader('Content-Type', 'application/json');
-	                        res.end(JSON.stringify(comment));
-	                    } else {
-	                        //Assume it's because of bad syntax, could we try catch for specific errors?
-	                        res.status(400).send("Could not update comment");
-	                    }
-	                });
-				} else {
-					res.status(404).send("Comment with this question id was not found");
-				}
+                    // update the content
+                    comment.content = req.body.content;
+                    comment.save(function (err) {
+                        if (!err) {
+                            res.setHeader('Content-Type', 'application/json');
+                            res.end(JSON.stringify(comment));
+                        } else {
+                            //Assume it's because of bad syntax, could we try catch for specific errors?
+                            res.status(400).send("Could not update comment");
+                        }
+                    });
+                } else {
+                    res.status(404).send("Comment with this question id was not found");
+                }
             } else {
                 res.status(404).send("Comment was not found");
             }
@@ -490,30 +490,30 @@ app.post("/questions/comments/:question_id", function (req, res) {
         errors = req.validationErrors();
 
     if (!errors) {
-		req.models.questions.get(req.params.question_id, function(err, question) {
-			// if there is no error, this question exists. so we can add a new answer
-			// to this question
-			if(!err) {
-				req.models.comments_question.create(
-					[{
-						date_posted: datetime,
-						content: req.body.content,
-						question_id: question.id
-					}], 
-					function (err, comment_created) {
-						if(!err) {
-		                    var comment = comment_created[0];
-		                    res.setHeader('Content-Type', 'application/json');
-		                    res.end(JSON.stringify(comment));
-						} else {
-							res.status(500).send(err);
-						}
-					}
-				);
-			} else {
-				res.status(400).send(errors);
-			}
-		});
+        req.models.questions.get(req.params.question_id, function (err, question) {
+            // if there is no error, this question exists. so we can add a new answer
+            // to this question
+            if (!err) {
+                req.models.comments_question.create(
+                    [{
+                        date_posted: datetime,
+                        content: req.body.content,
+                        question_id: question.id
+                    }],
+                    function (err, comment_created) {
+                        if (!err) {
+                            var comment = comment_created[0];
+                            res.setHeader('Content-Type', 'application/json');
+                            res.end(JSON.stringify(comment));
+                        } else {
+                            res.status(500).send(err);
+                        }
+                    }
+                );
+            } else {
+                res.status(400).send(errors);
+            }
+        });
     } else {
         res.status(400).send(errors);
     }
@@ -625,7 +625,7 @@ app.post("/answers", function (req, res) {
                 content: req.body.content,
                 owner_id: null,
                 question_id: null
-            }, ],
+            }],
             function (err, answer_created) {
                 if (!err) {
                     var answer = answer_created[0];
@@ -683,19 +683,18 @@ app.get('/answers/comments', function (req, res) {
 });
 
 app.get('/answers/comments/:answer_id', function (req, res) {
-	// this gets the question, and then we need to get the comments from that.
+    // this gets the question, and then we need to get the comments from that.
     req.models.answer.get(req.params.answer_id, function (err, answer) {
         if (!err) {
             res.setHeader('Content-Type', 'application/json');
-			
-			// we have the question.
-			answer.getComments(function(err, comments) {
-				if(!err) {
-					res.end(JSON.stringify(comments))
-				} else {
-					res.status(404).send('Comment not found');
-				}
-			});
+            // we have the question.
+            answer.getComments(function (err, comments) {
+                if (!err) {
+                    res.end(JSON.stringify(comments));
+                } else {
+                    res.status(404).send('Comment not found');
+                }
+            });
         } else {
             res.status(404).send('Comment not found');
         }
@@ -703,21 +702,19 @@ app.get('/answers/comments/:answer_id', function (req, res) {
 });
 
 app.get('/answers/comments/:answer_id/:comment_id', function (req, res) {
-	// this gets the question, and then we need to get the comments from that.
+    // this gets the question, and then we need to get the comments from that.
     req.models.comments_answer.get(req.params.comment_id, function (err, comment) {
         if (!err) {
             res.setHeader('Content-Type', 'application/json');
-			
-			// we have the answer, verify this is from the right question
-			comment.getOwner(function(err, owner) {
-				if(!err) {
-		            res.end(JSON.stringify(comment));
-				}
-				else {
-					res.status(404).send('Comment not found');
-				}
-			});
-			
+
+            // we have the answer, verify this is from the right question
+            comment.getOwner(function (err, owner) {
+                if (!err) {
+                    res.end(JSON.stringify(comment));
+                } else {
+                    res.status(404).send('Comment not found');
+                }
+            });
         } else {
             res.status(404).send('Comment not found');
         }
@@ -735,25 +732,25 @@ app.put('/answers/comments/:answer_id/:comment_id', function (req, res) {
     if (!errors) {
         req.models.comments_answer.get(req.params.comment_id, function (err, comment) {
             if (!err) {
-				
-				// verify this comment belongs to the question
-				if(comment.id == req.params.answer_id) {
 
-					// update the content
-	                comment.content = req.body.content;
-					
-	                comment.save(function (err) {
-	                    if (!err) {
-	                        res.setHeader('Content-Type', 'application/json');
-	                        res.end(JSON.stringify(comment));
-	                    } else {
-	                        //Assume it's because of bad syntax, could we try catch for specific errors?
-	                        res.status(400).send("Could not update comment");
-	                    }
-	                });
-				} else {
-					res.status(404).send("Comment with this answer id was not found");
-				}
+                // verify this comment belongs to the question
+                if (comment.id === req.params.answer_id) {
+
+                    // update the content
+                    comment.content = req.body.content;
+
+                    comment.save(function (err) {
+                        if (!err) {
+                            res.setHeader('Content-Type', 'application/json');
+                            res.end(JSON.stringify(comment));
+                        } else {
+                            //Assume it's because of bad syntax, could we try catch for specific errors?
+                            res.status(400).send("Could not update comment");
+                        }
+                    });
+                } else {
+                    res.status(404).send("Comment with this answer id was not found");
+                }
             } else {
                 res.status(404).send("Comment was not found");
             }
@@ -773,30 +770,30 @@ app.post("/answers/comments/:answer_id", function (req, res) {
         errors = req.validationErrors();
 
     if (!errors) {
-		req.models.answer.get(req.params.answer_id, function(err, answer) {
-			// if there is no error, this question exists. so we can add a new answer
-			// to this question
-			if(!err) {
-				req.models.comments_answer.create(
-					[{
-						date_posted: datetime,
-						content: req.body.content,
-						answer_id: answer.id
-					}], 
-					function (err, comment_created) {
-						if(!err) {
-		                    var comment = comment_created[0];
-		                    res.setHeader('Content-Type', 'application/json');
-		                    res.end(JSON.stringify(comment));
-						} else {
-							res.status(500).send(err);
-						}
-					}
-				);
-			} else {
-				res.status(400).send(errors);
-			}
-		});
+        req.models.answer.get(req.params.answer_id, function (err, answer) {
+            // if there is no error, this question exists. so we can add a new answer
+            // to this question
+            if (!err) {
+                req.models.comments_answer.create(
+                    [{
+                        date_posted: datetime,
+                        content: req.body.content,
+                        answer_id: answer.id
+                    }],
+                    function (err, comment_created) {
+                        if (!err) {
+                            var comment = comment_created[0];
+                            res.setHeader('Content-Type', 'application/json');
+                            res.end(JSON.stringify(comment));
+                        } else {
+                            res.status(500).send(err);
+                        }
+                    }
+                );
+            } else {
+                res.status(400).send(errors);
+            }
+        });
     } else {
         res.status(400).send(errors);
     }
